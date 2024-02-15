b813623a8394fe278073058c9fc7d67cec900353
ChristianTStu <christiantstu@gmail.com>
2024-02-14 22:33:53 -0600

    Add standard settings and import

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,9 +2,13 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
 
+    # Overall class to manage game assets and behvior.
     def __init__(self):
+        # Initialize the game, and creat game resources.
         pygame.init()
         self.clock = pygame.time.Clock()
 
@@ -15,8 +19,9 @@ class AlienInvasion:
         self.bg_color = (230, 230, 230)
 
     def run_game(self):
-        
+        # Start the main loop for the game.
         while True:
+            # Watch for keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
@@ -24,10 +29,12 @@ class AlienInvasion:
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.bg_color)
 
+            # Make the most recently drawn screen visible.
             pygame.display.flip()
             self.clock.tick(60)
 
 if __name__ == '__main__':
+    # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
 
